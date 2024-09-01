679bbf3acb07473dbb1f9de82341d738e5b65d87
Diego Enriquez <astrodev07@proton.me>
2024-09-01 02:08:22 +0200

    Version: 0.1 Beta Completed: -distanceConversion Function
    To-Do:
    -weightConversion Function
    -timeConversion Function
    -tempConversionFunction

---
+++ main.js
@@ -0,0 +1,66 @@
+console.log("Unit Converter:")
+
+/* 
+    This Function ask for 3 parameters, `distance`, `from`, `to`
+    
+        distance: the distance in number to convert
+        Example: 1000, 10, 999, or another number
+
+        from: the unit that you want to convert from
+        Example: m, dm, cm, km, or another unit
+
+        to: the unit that you want to convert to
+        Example: m, dm, cm, km, or another unit
+
+        Available Units:
+        mm ==> milimeters
+        cm ==> centimeters
+        dm ==> decimeters
+        m ==> meters
+        dam ==> decameter
+        hm ==> hetometer
+        km ==> kilometer
+
+*/
+let distanceConversion = (distance, from, to) => {
+    // First, lets handle some errors
+    if(isNaN(distance)){
+        console.error("Distance, needs to be a number")
+        return 1;
+    }
+    else if(typeof(from)!='string' || typeof(to)!='string'){
+        console.error("The type of From, and To parameters, has to be string")
+    }
+    // Now, lets make the logic
+    else{
+        // I'm going to store all the distance units in an array
+        let units = ['mm', 'cm', 'dm', 'm', 'dam', 'hm', 'km']
+        // Now, let's know if we have to multiply or divide
+        if(units.indexOf(from)<units.indexOf(to)){
+            // We have to divide
+            let howManyZeroes=units.indexOf(to)-units.indexOf(from)
+            let divideBy='1'
+            for(let i=0; i<howManyZeroes; i++){
+                divideBy+='0'
+            }
+
+            console.log(`${distance}${from} are ${distance/divideBy}${to}`)
+        }
+        else if(units.indexOf(from)>units.indexOf(to)){
+            // we have to divide
+            let howManyZeroes=units.indexOf(from)-units.indexOf(to)
+            let multiplyBy='1'
+            for(let i=0; i<howManyZeroes; i++){
+                multiplyBy+='0'
+            }
+
+            console.log(`${distance}${from} are ${distance*multiplyBy}${to}`)
+        }
+        else {
+            //we dont need to do nothing, from and to have the same value
+            console.error("From and To have to be different")
+        }
+    }
+};
+
+distanceConversion(1, 'm', 'mm')
